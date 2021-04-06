09a48faeee8fd8e6e22ce3afdd768c2b661766d7
Shubham Soni <shubham.soni@mtxb2b.com>
2021-04-06 19:42:04 +0530

    Added Rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,38 @@
-import { DynamicModule, MiddlewareConsumer, Module } from '@nestjs/common';
+import { DynamicModule, MiddlewareConsumer } from '@nestjs/common';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BaseModule } from './base/base.module';
 import { DomainModule } from './domain/domain.module';
+import { APP_GUARD } from '@nestjs/core';
+const Redis = require('ioredis');
 
+const cluster = new Redis({
+  host: 'localhost',
+  port: 6379,
+});
 export class AppModule {
   static forRoot(): DynamicModule {
-    const modulesToImport = [BaseModule, DomainModule];
+    const modulesToImport = [
+      ThrottlerModule.forRoot({
+        ttl: 10,
+        limit: 30,
+        storage: new ThrottlerStorageRedisService(cluster),
+      }),
+      BaseModule,
+      DomainModule,
+    ];
     return {
       module: AppModule,
       imports: modulesToImport,
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: APP_GUARD,
+          useClass: ThrottlerGuard,
+        },
+      ],
       controllers: [AppController],
     };
   }
