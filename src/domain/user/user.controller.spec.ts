6df8ae4779c2367118bad3addefd943f5320d207
Shubham Soni <shubham.soni@mtxb2b.com>
2021-04-08 11:30:20 +0530

    Added test case for user controller

--- src/domain/user/user.controller.spec.ts
+++ src/domain/user/user.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BaseModule } from '../../base/base.module';
 import { UserController } from './user.controller';
+import { UserModule } from './user.module';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
+      imports: [BaseModule, UserModule],
     }).compile();
 
     controller = module.get<UserController>(UserController);
